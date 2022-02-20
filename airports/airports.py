f7c26195f84cdce28bea95f12657a7a88d8ccc9b
browneye1826 <59311049+browneye1826@users.noreply.github.com>
2022-02-20 00:42:02 -0500

    General updates

--- airports/airports.py
+++ airports/airports.py
@@ -1,4 +1,3 @@
-import re
 import pandas as pd
 import streamlit as st
 
@@ -23,17 +22,17 @@ st.sidebar.header("Please filter here:")
 cols = st.sidebar.multiselect(
     "Select columns to show",
     options=list(df.columns),
-    default=["iso_country", "iata_code"],
+    default=["municipality", "iso_country", "name", "iata_code"],
 )
 
 country = st.sidebar.multiselect(
-    "Filter country", options=df["iso_country"].unique(), default=None
+    "Filter country", options=df["iso_country"].unique(), default="US"
 )
 ap_type = st.sidebar.multiselect(
-    "Filter airport type", options=df["type"].unique(), default=None
+    "Filter airport type", options=df["type"].unique(), default="large_airport"
 )
 
-l1 = st.sidebar.text_input("Filter by first letter of IATA")
+l1 = st.sidebar.text_input("Filter by first letter of IATA", value="A")
 l2 = st.sidebar.text_input("Filter by second letter of IATA")
 l3 = st.sidebar.text_input("Filter by third letter of IATA")
 
@@ -43,15 +42,25 @@ df_selection = df.query(
 )
 
 #  --- MAIN PAGE ---
-st.title(":airplane:  Global Airport Data")
-st.text(" ")
+st.title("Global Airport Data :airplane:")
+st.write("***")
+st.write("\n")
 
 if st.checkbox("Show full raw data"):
     st.subheader("All raw data")
     st.dataframe(df)
 
 st.text(" ")
+st.write(
+    "Currently showing `{}` in `{}` with letters `{}{}{}` in `iata_code`:".format(
+        ", ".join(ap_type), ", ".join(country), l1, l2, l3
+    )
+)
 st.dataframe(df_selection[cols])
 
 with st.expander("Show map"):
     st.map(df_selection.rename(columns={"longitude_deg": "lon", "latitude_deg": "lat"}))
+
+
+st.write("\n\n")
+st.caption("[Data source](https://ourairports.com/data/)")
